dbab5b594a5f8855bc251916ef472e179893553e
Andres Felipe Estrada <90068402+AndresFelipeEstrada@users.noreply.github.com>
2024-11-23 10:35:24 -0500

    ADD: bookingMachine.js

--- src/machines/bookingMachine.js
+++ src/machines/bookingMachine.js
@@ -1,30 +1,101 @@
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
+import { fetchCountries } from "../utils/api";
 
-export const bookingMachine = createMachine({
-  id: "buy plane tickets",
-  initial: "initial",
+const fillCountries = {
+  initial: "loading",
   states: {
-    initial: {
-      on: {
-        START: "search",
+    loading: {
+      invoke: {
+        id: "getCountries",
+        src: () => fetchCountries,
+        onDone: {
+          target: "success",
+          actions: assign({ countries: ({ event }) => event.data }),
+        },
+        onError: {
+          target: "failure",
+          actions: assign({
+            error: "Fallo el request",
+          }),
+        },
       },
-    },
-    search: {
-      on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
-      },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: "initial",
-      },
-    },
-    tickets: {
-      on: {
-        FINISH: "initial",
+      success: {},
+      failure: {
+        on: {
+          RETRY: { target: "loading" },
+        },
       },
     },
   },
-});
+};
+
+export const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "",
+      countries: [],
+      error: "",
+    },
+    states: {
+      initial: {
+        entry: assign({
+          passengers: ({ context }) => (context.passengers = []),
+          selectedCountry: ({ context }) => (context.selectedCountry = ""),
+        }),
+        on: {
+          START: {
+            target: "search.loading",
+          },
+        },
+      },
+      search: {
+        initial:'loading',
+        states: fillCountries,
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: assign({
+              selectedCountry: ({ event }) => event.selectedCountry,
+            }),
+          },
+          CANCEL: "initial",
+        },
+      },
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+          ADD: {
+            target: "passengers",
+            actions: assign({
+              passengers: ({ event, context }) => [
+                ...context.passengers,
+                event.newPassenger,
+              ],
+            }),
+          },
+        },
+      },
+      tickets: {
+        on: {
+          FINISH: "initial",
+          CANCEL: "initial",
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      cleanContext: assign({
+        selectedCountry: "",
+        passengers: [],
+      }),
+    },
+  },
+);
